7dd22c8094bfcdacb0b94b68bda7cf1dd444eb38
visualstudio <visualstudio@230-PC38>
2019-03-06 09:02:05 -0500

    circle

--- C0749419/js/main.js
+++ C0749419/js/main.js
@@ -1,1 +1,37 @@
-﻿// Your code here!
+﻿/*const circle = {
+        radius: 1,
+        location: {
+            x: 1,
+            y: 1
+        },
+        draw: function () {
+            console.log('draw');
+
+        }
+    };
+
+    circle.draw();
+
+         // factory function
+    function createCircle(radius) {
+        return {
+            radius,
+            draw: function () {
+                console.log('drawxxx');
+            }
+        };
+    }
+        const circle = createCircle(1);
+        circle.draw();*/
+
+//Constructor function
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+
+}
+
+const another = new Circle(1);
